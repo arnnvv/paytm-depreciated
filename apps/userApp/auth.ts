b28dadd48b1a7655c31c9d00367baa538463db64
arnnvv <arnav.900@protonmail.com>
2024-03-27 16:23:01 +0530

    auth

--- apps/userApp/auth.ts
+++ apps/userApp/auth.ts
@@ -1,5 +1,75 @@
 import db from "@repo/db/client";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
+import { validate } from "./validate";
 
-export const authOptions = {};
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        number: {
+          label: "Phone No",
+          type: "text",
+          placeholder: "1234567890",
+          required: true,
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "**********",
+          required: true,
+        },
+      },
+      async authorize(credentials: any) {
+        if (!validate(credentials)) return null;
+        //OTP validation
+        const hash = await bcrypt.hash(credentials?.password, 10);
+        const user = await db.user.findFirst({
+          where: {
+            number: credentials?.number,
+          },
+        });
+        if (user) {
+          const passwordValidate = await bcrypt.compare(
+            credentials.password,
+            user.password,
+          );
+          if (passwordValidate) {
+            return {
+              id: user.id.toString(),
+              name: user.name,
+              email: user.email,
+            };
+          }
+          return null;
+        }
+        try {
+          const user = await db.user.create({
+            data: {
+              number: credentials.phone,
+              password: hash,
+            },
+          });
+
+          console.log(`User created: ${user}`);
+          return {
+            id: user.id.toString(),
+            name: user.name,
+            email: user.number,
+          };
+        } catch (error) {
+          console.error(`Error in validation: ${error}`);
+        }
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
+  callbacks: {
+    async session({ token, session }: any) {
+      session.user.id = token.sub;
+      return session;
+    },
+  },
+};
---
+++ apps/userApp/validate.ts
@@ -0,0 +1,14 @@
+import * as v from "valibot";
+
+const signUpIn = v.object({
+  name: v.optional(v.string()),
+  email: v.optional(v.string([v.email()])),
+  phone: v.string([v.length(10)]),
+  password: v.string([v.minLength(10)]),
+});
+export type SignUpIn = v.Output<typeof signUpIn>;
+
+export const validate = (data: SignUpIn) => {
+  const result = v.safeParse(signUpIn, data);
+  return result.success;
+};
