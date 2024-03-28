49f781c3e9e38016df8c9b0578bbf0891d1c6673
arnnvv <arnav.900@protonmail.com>
2024-03-28 13:56:20 +0530

    wrkng

--- apps/userApp/auth.ts
+++ apps/userApp/auth.ts
@@ -1,5 +1,7 @@
 import db from "@repo/db/client";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 import bcrypt from "bcrypt";
 import { validate } from "./validate";
 
@@ -64,8 +66,16 @@ export const authOptions = {
         return null;
       },
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
   ],
-  secret: process.env.JWT_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ token, session }: any) {
       session.user.id = token.sub;
