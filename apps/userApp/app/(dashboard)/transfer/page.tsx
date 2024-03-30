ff42f76a1155b5ad5a7dbdad9753673932c244f9
arnnvv <arnav.900@protonmail.com>
2024-03-30 22:07:32 +0530

    transaction

--- apps/userApp/app/(dashboard)/transfer/page.tsx
+++ apps/userApp/app/(dashboard)/transfer/page.tsx
@@ -1,7 +1,57 @@
-export default function Transfer(): JSX.Element {
+import db from "@repo/db/client";
+import { AddMoney } from "../../../components/AddMoneyCard";
+import { BalanceCard } from "../../../components/BalanceCard";
+import { OnRampTransactions } from "../../../components/OnRampTransaction";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../auth";
+
+const getBalance = async () => {
+  const session = await getServerSession(authOptions);
+  const balance = await db.balance.findFirst({
+    where: {
+      userId: session?.user?.id,
+    },
+  });
+  return {
+    balance: balance?.amount,
+    locked: balance?.locked,
+  };
+};
+
+const getOnRampTransactions = async () => {
+  const session = await getServerSession(authOptions);
+  const transactions = await db.onRampTransaction.findMany({
+    where: {
+      userId: session?.user?.id,
+    },
+  });
+  return transactions.map((transaction) => ({
+    time: transaction.startTime,
+    amount: transaction.amount,
+    status: transaction.status,
+    provider: transaction.provider,
+  }));
+};
+export default async function Transfer(): Promise<JSX.Element> {
+  const balance = await getBalance();
+  const transactions = await getOnRampTransactions();
+
   return (
-    <div>
-      <h1>Transfer</h1>
+    <div className="w-screen">
+      <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+        Transfer
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+        <div>
+          <AddMoney />
+        </div>
+        <div>
+          <BalanceCard amount={balance.amount} locked={balance.locked} />
+          <div className="pt-4">
+            <OnRampTransactions transactions={transactions} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
