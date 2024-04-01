d373e206305abd4158cbc8b3cec5bcdfe088df47
arnnvv <arnav.900@protonmail.com>
2024-04-02 01:31:02 +0530

    sendingMoney

---
+++ apps/userApp/actions/p2ptransfer.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import db from "@repo/db/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+
+export const p2pTransfer = async (to: string, amount: number) => {
+  const session = await getServerSession(authOptions);
+
+  const from = session?.user?.id;
+  if (!from) throw new Error("Not logged in");
+
+  const toExist = await db.user.findFirst({
+    where: {
+      number: to,
+    },
+  });
+  if (!toExist) throw new Error("User does not exist");
+
+  await db.$transaction(async (tx) => {
+    const fromBalance = await tx.balance.findFirst({
+      where: {
+        userId: from,
+      },
+    });
+    if (!fromBalance || fromBalance.amount < amount)
+      throw new Error("Insufficient balance");
+    await tx.balance.update({
+      where: {
+        userId: from,
+      },
+      data: {
+        amount: {
+          decrement: amount,
+        },
+      },
+    });
+    await tx.balance.update({
+      where: {
+        userId: toExist.id,
+      },
+      data: {
+        amount: {
+          increment: amount,
+        },
+      },
+    });
+  });
+};
---
+++ apps/userApp/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,9 @@
+import { SendCard } from "./../../../components/SendCard";
+
+export default function Send() {
+  return (
+    <div className="w-full h-full">
+      <SendCard />
+    </div>
+  );
+}
